3dc7acfbbff8deb6262c2492bd37d87a532904e3
Luis Andres Rojas Matey <larmcr@gmail.com>
2017-03-04 21:23:28 -0600

    BCR

--- js/entities.js
+++ js/entities.js
@@ -17,18 +17,6 @@ const entities = {
       return cleanData(TipoCambioCompra, TipoCambioVenta);
     }
   },
-  bncr: {
-    name: 'Banco Nacional de Costa Rica (BNCR)',
-    url: 'http://www.bncr.fi.cr/BNCR/TipoCambio.aspx',
-    prefix: true,
-    ready: true,
-    parse: (result) => {
-      const html = HTML(result);
-      const buy = $(html.select('.compra')[0]).select('span').text();
-      const sell = $(html.select('.venta')[0]).select('span').text();
-      return cleanData(buy, sell);
-    }
-  },
   bancredito: {
     name: 'Banco Crédito Agrícola de Cartago (Bancrédito)',
     url: 'https://www.bancreditocr.com/tipoCambio/TipoCambio.xml',
@@ -40,6 +28,31 @@ const entities = {
       // const buy = $(html.select('.compra')[0]).select('span').text();
       // const sell = $(html.select('.venta')[0]).select('span').text();
       // return cleanData(buy, sell);
+    }
+  },
+  bcr: {
+    name: 'Banco de Costa Rica (BCR)',
+    url: 'http://www.bancobcr.com/js/tipoCambio/BUS/actual_formato.asp?i=ES',
+    prefix: true,
+    ready: true,
+    parse: (result) => {
+      const html = HTML(result);
+      const rates = html.select('.tipocambio_currency');
+      const buy = $(rates[0]).text();
+      const sell = $(rates[1]).text();
+      return cleanData(buy, sell);
+    }
+  },
+  bncr: {
+    name: 'Banco Nacional de Costa Rica (BNCR)',
+    url: 'http://www.bncr.fi.cr/BNCR/TipoCambio.aspx',
+    prefix: true,
+    ready: true,
+    parse: (result) => {
+      const html = HTML(result);
+      const buy = $(html.select('.compra')[0]).select('span').text();
+      const sell = $(html.select('.venta')[0]).select('span').text();
+      return cleanData(buy, sell);
     }
   },
 };
